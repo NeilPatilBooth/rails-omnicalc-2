1463a9cd20acc91eb868a482d5605bb03f808d4d
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-11-02 23:55:03 +0000

    subtraction works

--- app/controllers/subtraction_controller.rb
+++ app/controllers/subtraction_controller.rb
@@ -3,7 +3,7 @@ class SubtractionController < ApplicationController
     render({ :template => "subtraction_templates/subtraction_form" })
   end
 
-  def subtract_these
+  def subtract
     @first_number = params.fetch("first_num").to_f
     @second_number = params.fetch("second_num").to_f
 
